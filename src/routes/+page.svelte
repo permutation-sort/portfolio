398aea85cee6fbcf5b3d7bc67c303b68c626ed8d
mattwbdv <koenigmattd@gmail.com>
2023-05-16 22:09:53 -0700

    fixing nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
-<div class="navbar bg-base-100" style="position: fixed;">
+<div class="navbar bg-base-100" style="position: relative;">
   <div class="flex-1">
     <a class="btn btn-ghost normal-case text-xl" href="/">@permutation-sort</a>
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1">
-      <li tabindex="0">
+      <li tabindex="0" style="position: relative;">
         <a>
           my work
           <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
             <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/>
           </svg>
         </a>
-        <ul class="p-2 bg-base-100">
+        <ul class="p-2 bg-base-100" style="position: absolute; z-index: 999;">
           <li><a href="/resume">resume</a></li>
           <li><a href="https://github.com/permutation-sort">github</a></li>
           <li><a href="https://www.linkedin.com/in/mattdk/">linkedin</a></li>
@@ -21,7 +21,6 @@
     </ul>
   </div>
 </div>
-
 
     <div class="hero min-h-screen bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900">
       <div class="flex flex-col justify-center items-center">
